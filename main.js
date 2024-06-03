16843a5ba318ae21aa564a015b1f7fd31fa77018
nsreenjo <nsreenjoundya70@gmail.com>
2024-06-03 23:34:14 +0300

    js

---
+++ main.js
@@ -0,0 +1,147 @@
+for(let i=1;i<=10;i++)
+    console.log(i)
+//// 2/////22222222222222222222222222222222222
+console.log("task2");
+let i=1;
+while (i<=10){
+    console.log(i);
+    i++
+}
+//// 3/////3333333333333333333333333333333333
+console.log("task3");
+
+// used while
+
+let u=1
+while (u <= 10) {
+    if (u % 2 === 0) {
+        console.log(u);
+    }
+    u++;
+}
+
+// used for
+
+for(let o=1 ;o<=10 ;o++)
+    if(o%2===0){
+    console.log(o);
+}
+// 5/////555555555555555555555555555555555
+console.log("task5");
+
+
+// let sum=0;
+// for(var q=1 ; q<=10;q++)
+//     {
+//         console.log(q)
+//         sum += q;
+//     }
+
+let sum=0;
+for (let t=1;t<=10;t ++)
+    {
+    sum+=t
+}
+console.log(sum);
+
+// 6////////////66666666666666666666666
+console.log("task6");
+
+for(let d=1 ;d<=10 ;d++)
+    if(d%2===1){
+    console.log(d);
+}
+
+
+// 7/////7777777777777777777777777777
+console.log("task7");
+
+let h=28;
+if (h>=16){
+    console.log("You are an adult")
+}
+else
+{
+    console.log("You arent an adult")
+}
+
+// 8/////888888888888888888888
+console.log("task8");
+let score=45;
+if (score<=50){
+    console.log("You failed the test")
+}
+
+// 9/////99999999999999999999999
+console.log("task9");
+
+let Name="john";
+if (Name<="john"){
+    console.log("hi john")
+}
+
+// 10/////10 10 10 10 10 10 10 10 10 10 
+console.log("task10");
+
+let day = "monday"
+
+if(day !=="saturday" && day !== "friday"){
+    console.log("It's a weekday"); 
+}
+///////////////////////////////////////////////....
+
+// let dayy = "Monday"; 
+
+// switch (dayy) {
+//     case "Saturday":
+//     case "Friday":
+//         break;
+//     default:
+//         console.log("It's a weekday");
+//         break;
+// }
+// 11/////11 11 11 11 11 11 11 11 11 111 11 11 
+console.log("task-11");
+
+var num = 10; 
+
+if (num % 2 === 0) {
+    console.log("The number is even");
+}
+
+
+
+// let num=4;
+// for ( let n=1 ;n>=1;n++){
+//     if (num%2 == 0){
+//     console.log("the number is even")
+    
+// }}
+
+// 12////12 12 12 12 12 12 12 12 12 12 12 12 12 12 
+console.log("task-12");
+
+let x= 5;
+if (x>=0){
+    console.log("is a positive number");
+}
+
+// 13////13 13 13 13 13 13 13 13 13 13 13 13
+console.log("task-13");
+
+let p= -5;
+if (p<=0){
+    console.log("is a negative number");
+}
+ 
+///////////////////////////////////////////////
+console.log("task-14");
+
+let gap= 3.5;
+if (gap>=3.5){
+    console.log("you have a good gap");}
+
+
+
+
+
